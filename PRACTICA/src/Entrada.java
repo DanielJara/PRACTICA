8bf3ee0e04700af27043804c53a7204791b98902
Daniel Jara <danieljara773@gmail.com>
2018-12-11 12:31:17 +0100

    PUNTO3

---
+++ PRACTICA/src/Entrada.java
@@ -0,0 +1,9 @@
+
+public class Entrada {
+
+	public static String cadena() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- PRACTICA/src/Reloj.java
+++ PRACTICA/src/Reloj.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Reloj {
 
@@ -10,6 +11,41 @@ public class Reloj {
 		hora = 12;
 		minutos = 36;
 		segundos = 52;
+	}
+	
+	public void Print() {
+		int numEquipos = 5;
+
+		ArrayList<String> liga = new ArrayList<>();
+		ArrayList<String> liga2 = new ArrayList<>();
+
+		System.out.println();
+
+		for(int i = 0; i < numEquipos; i++) {
+			System.out.print("Dime el equipo número " + (i + 1) + ": ");
+			liga.add(Entrada.cadena());
+		}
+
+		liga2 =  liga;
+
+		System.out.println("\n\tArrayList liga: " + liga);
+		System.out.println("\tArrayList liga2: " + liga2);
+
+		System.out.println("\n\tCantidad de valores del ArrayList liga: " + liga.size());
+
+		liga.remove(4);
+		System.out.println("\n\tPosición 4 del ArrayList liga borrado: " + liga);
+
+		System.out.print("\nDime un nuevo equipo para la posición 2 del ArrayList: ");
+		String posicion2 = Entrada.cadena();
+		liga.set(2, posicion2);
+		System.out.println("\n\tosicón 2 del ArrayList liga sustituido: " + liga);
+
+		System.out.print("\nDime el equipo que esta en la posicion 3 del Arralist: ");
+		String posicion3 = Entrada.cadena();
+		liga.remove(posicion3);
+		System.out.println("\n\tArrayList liga con la posición 3 borrada: " + liga);
+
 	}
 
 	
