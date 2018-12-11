f5a07ace3cc48e0ea9bbf419f1376b6d42a92878
Daniel Jara <danieljara773@gmail.com>
2018-12-11 11:55:49 +0100

    PUNTO1

---
+++ PRACTICA/src/Daniel.java
@@ -0,0 +1,43 @@
+
+public class Daniel {
+
+	String nombre;
+	String apellidos;
+	String dni;
+	
+	
+	public Daniel() {
+		nombre = "Daniel";
+		apellidos = "Jara Palacios";
+		dni = "12345678A";
+	}
+
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
+	
+}
---
+++ PRACTICA/src/Reloj.java
@@ -0,0 +1,43 @@
+
+public class Reloj {
+
+	int hora;
+	int minutos;
+	int segundos;
+	
+	public Reloj() {
+		
+		hora = 12;
+		minutos = 36;
+		segundos = 52;
+	}
+
+	
+	
+	
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+	
+	
+}
